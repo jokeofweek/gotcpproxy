f38f43d254c3d41604721cb0901279ffee277621
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2013-09-22 08:46:19 -0400

    Added fixes.

--- proxy.go
+++ proxy.go
@@ -7,14 +7,13 @@ import (
     "net"
 )
 
-
 var fromHost = flag.String("from", "localhost:80", "The proxy server's host.")
 var toHost = flag.String("to", "localhost:8000", "The host that the proxy " +
     " server should forward requests to.")
 var maxConnections = flag.Int("c", 25, "The maximum number of active " +
     "connection at any given time.")
-
-
+var maxWaitingConnections = flag.Int("cw", 10000, "The maximum number of " +
+    "connections that can be waiting to be served.")
 
 func main() {
     // Parse the command-line arguments.
@@ -30,7 +29,7 @@ func main() {
     }
 	
 	// The channel of connections which are waiting to be processed.
-    waiting := make(chan net.Conn)
+    waiting := make(chan net.Conn, *maxWaitingConnections)
     // The booleans representing the free active connection spaces.
     spaces := make(chan bool, *maxConnections)
     // Initialize the spaces
@@ -85,34 +84,46 @@ func handleConnection(connection net.Conn) {
     }
     defer remote.Close()
 	
-	// Create our channel which waits for completion, and start both copying
-    // goroutines.
-    complete := make(chan bool)
-    go copyContent(connection, remote, complete)
-    go copyContent(remote, connection, complete)
-    // Block until we've completed!
+	// Create our channel which waits for completion, and our two channels to
+    // signal that a goroutine is done.
+    complete := make(chan bool, 2)
+    ch1 := make(chan bool, 1)
+    ch2 := make(chan bool, 1)
+    go copyContent(connection, remote, complete, ch1, ch2)
+    go copyContent(remote, connection, complete, ch2, ch1)
+    // Block until we've completed both goroutines!
+    <- complete
     <- complete
 }
 
-func copyContent(from net.Conn, to net.Conn, complete chan bool) {
+func copyContent(from net.Conn, to net.Conn, complete chan bool, done chan bool, otherDone chan bool) {
     var err error = nil
     var bytes []byte = make([]byte, 256)
     var read int = 0
     for {
-        // Read data from the source connection.
-        read, err = from.Read(bytes)
-        // If any errors occured, write to complete as we are done (one of the
-        // connections closed.)
-        if err != nil {
-            complete <- true
-            break
-        }
-        // Write data to the destination.
-        _, err = to.Write(bytes[:read])
-        // Same error checking.
-        if err != nil {
-            complete <- true
-            break
+        select {
+            // If we received a done message from the other goroutine, we exit.
+            case <- otherDone:
+                complete <- true
+                break
+            default:
+                // Read data from the source connection.
+                read, err = from.Read(bytes)
+                // If any errors occured, write to complete as we are done (one of the
+                // connections closed.)
+                if err != nil {
+                    complete <- true
+                    done <- true
+                    break
+                }
+                // Write data to the destination.
+                _, err = to.Write(bytes[:read])
+                // Same error checking.
+                if err != nil {
+                    complete <- true
+                    done <- true
+                    break
+                }
         }
     }
 }
